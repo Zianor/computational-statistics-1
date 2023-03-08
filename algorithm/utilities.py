bf14a9919dcf1a92b4faf762b81a5e85837cf153
Zianor <zianor@zianor.de>
2023-03-08 12:56:09 +0100

    Put a 'classical sortnregress' function in utilities

--- algorithm/utilities.py
+++ algorithm/utilities.py
@@ -1,6 +1,8 @@
 # visualize the matrix as a directed graph
 import networkx as nx
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression, LassoLarsIC
+import numpy as np
 
 def graph(W):
     G = nx.DiGraph(W)
@@ -8,3 +10,20 @@ def graph(W):
     pos = nx.circular_layout(G)
     nx.draw(G, pos, with_labels=True, node_color='w', node_size=1000, font_size=20)
     plt.show()
+
+
+def sortnregress_classic(X):
+    LR = LinearRegression()
+    LL = LassoLarsIC(criterion='bic')
+
+    d = X.shape[1]
+    W = np.zeros((d, d))
+    increasing = np.argsort(np.var(X, axis=0))
+    for k in range(1, d):
+        covariates = increasing[:k]
+        target = increasing[k]
+        LR.fit(X[:, covariates], X[:, target].ravel())
+        weight = np.abs(LR.coef_)
+        LL.fit(X[:, covariates] * weight, X[:, target].ravel())
+        W[covariates, target] = LL.coef_ * weight
+    return W
