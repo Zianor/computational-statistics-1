0527e8d11938a53e64535e95c61cdacc2947a99e
Zianor <zianor@zianor.de>
2023-03-08 16:00:43 +0100

    Change alpha for fixed trial

--- algorithm/main.py
+++ algorithm/main.py
@@ -35,7 +35,7 @@ def main():
     best_individual = objective(
         optuna.trial.FixedTrial(
             {
-                "alpha_factor": 0.3,
+                "alpha_factor": 0.1,
                 "use_cluster_inits": True,
                 "fit_intercept": fit_intercept,
                 "n_pop": 50,
